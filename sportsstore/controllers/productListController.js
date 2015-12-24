25eb7f821bc0de1b2d39190fee29cef7039b68db
trojansaga <trojansaga@naver.com>
2015-12-24 21:49:03 +0900

    first commit

--- sportsstore/controllers/productListController.js
+++ sportsstore/controllers/productListController.js
@@ -1,7 +1,7 @@
 angular.module("sportsStore")
 .constant("productListActiveClass", "btn-primary")
 .constant("productListPageCount", 3)
-.controller("productListCtrl", function ($scope, $filter, productListActiveClass, productListPageCount) {
+.controller("productListCtrl", function ($scope, $filter, productListActiveClass, productListPageCount, cart) {
 	var selectedCategory = null;
 
 	$scope.selectedPage = 1;
@@ -17,7 +17,7 @@ angular.module("sportsStore")
 	}
 
 	$scope.categoryFilterFn = function (product) {
-		return selectedCategory == null || 
+		return selectedCategory == null ||
 		product.category == selectedCategory;
 	}
 
@@ -28,4 +28,8 @@ angular.module("sportsStore")
 	$scope.getPageClass = function (page) {
 		return $scope.selectedPage = page ? productListActiveClass : "";
 	}
+	$scope.addProductToCart = function (product) {
+		cart.addProduct(product.id, product.name, product.price);
+	}
+
 });
